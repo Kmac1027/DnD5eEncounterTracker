a48442b52a1b441b51476fec04df5e9dc6e73929
Kevin McNeil <kevin.michael.mcneil@gmail.com>
2022-07-13 13:55:31 -0700

    Updates

---
+++ precache-manifest.39f18b57f9931f76ecf011c444ef266d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "788fc3afc0a2ca99c65a",
+    "url": "/DnD5eEncounterTracker/static/css/main.4c331d5c.chunk.css"
+  },
+  {
+    "revision": "788fc3afc0a2ca99c65a",
+    "url": "/DnD5eEncounterTracker/static/js/main.8a1bbee0.chunk.js"
+  },
+  {
+    "revision": "446bf10efe3bdf1d477e",
+    "url": "/DnD5eEncounterTracker/static/js/runtime~main.9df4d7ec.js"
+  },
+  {
+    "revision": "94b533234a19088812e7",
+    "url": "/DnD5eEncounterTracker/static/css/2.ea1a997c.chunk.css"
+  },
+  {
+    "revision": "94b533234a19088812e7",
+    "url": "/DnD5eEncounterTracker/static/js/2.acc0dae3.chunk.js"
+  },
+  {
+    "revision": "242f9cb65021bb3dbf9b",
+    "url": "/DnD5eEncounterTracker/static/js/3.5b5b5cc8.chunk.js"
+  },
+  {
+    "revision": "cbe6ae5edd3a3f237132e3351994e941",
+    "url": "/DnD5eEncounterTracker/static/media/statBG.cbe6ae5e.png"
+  },
+  {
+    "revision": "4024a14945dfc21b17fe20870df9d372",
+    "url": "/DnD5eEncounterTracker/index.html"
+  }
+];
