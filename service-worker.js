3be1181f1d32d87a912cd2d2671f11fff9d2756c
Kevin McNeil <kevin.michael.mcneil@gmail.com>
2022-07-22 20:03:41 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/DnD5eEncounterTracker/precache-manifest.f6379d82bc88f4c30a5ffb04d7e948e4.js"
+  "/DnD5eEncounterTracker/precache-manifest.96397bd0de55bdfb5f5e6dd99a6d191a.js"
 );
 
 workbox.clientsClaim();
